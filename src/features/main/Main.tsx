a530031a0ba238eb000e56a1bc842b151cbb08aa
Alexei <i0ver@mail.ru>
2022-12-15 23:55:45 +0300

    evelina

--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -4,17 +4,19 @@ import {Navigation} from "../navigation/Navigation";
 import {Sorting} from "../sorting/Sorting";
 
 export const Main = () => {
-    return <div className={style.container}>
-        <Navigation/>
-        <Sorting/>
-        <div className={style.main}>
-            <h1>Hello everybody!</h1>
-            <h2>click button!</h2>
-            <Link to={'/1'}>
-                <div className={style.btnStart}>Start</div>
-            </Link>
-        </div>
-        <Sorting/>
-        <Navigation/>
-    </div>
+    return <>
+        {/*<div className={style.container}>*/}
+            <Navigation/>
+        {/*    <Sorting/>*/}
+        {/*    <div className={style.main}>*/}
+        {/*        <h1>Hello everybody!</h1>*/}
+        {/*        <h2>click button!</h2>*/}
+        {/*        <Link to={'/1'}>*/}
+        {/*            <div className={style.btnStart}>Start</div>*/}
+        {/*        </Link>*/}
+        {/*    </div>*/}
+        {/*    <Sorting/>*/}
+        {/*    <Navigation/>*/}
+        {/*</div>*/}
+    </>
 }
